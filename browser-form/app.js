7d99a483d471b3214bb90e048a4a9237a84e6995
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-05-03 17:54:01 +0300

    Add zip code validation

--- browser-form/app.js
+++ browser-form/app.js
@@ -17,6 +17,10 @@ const checkElementValidity = function(element, elementContainer, regex, errorTex
   const errorElement = document.createElement('div');
   elementContainer.appendChild(errorElement);
 
+  if (elementContainer === zipContainer) {
+    errorElement.classList.add('zipError');
+  }
+
   window.addEventListener("load", () => {
     const isValid = regex.test(element.value);
     isValid ? element.classList.add('valid') : element.classList.add('invalid');
@@ -24,14 +28,21 @@ const checkElementValidity = function(element, elementContainer, regex, errorTex
   
   element.addEventListener("input", () => {
     const isValid = regex.test(element.value);
+    const isBlank = element.value === '';
+    
     if (isValid) {
       element.classList.add('valid');
+      element.classList.remove('invalid');
       errorElement.textContent = "";
       errorElement.classList.remove('error');
     } else {
       element.classList.add('invalid');
+      element.classList.remove('valid');
       errorElement.classList.add('error');
-      errorElement.textContent = "Please enter a correct email address";
+      errorElement.textContent = errorText;
+      if (isBlank) {
+        errorElement.textContent = "This field can't be blank";
+      }
     }
   });
   
@@ -41,6 +52,7 @@ const checkElementValidity = function(element, elementContainer, regex, errorTex
     const isValid = regex.test(element.value);
     const isBlank = element.value === '';
     if (!isValid) {
+      element.classList.remove('valid');
       element.classList.add('invalid');
       errorElement.textContent = errorText;
       errorElement.classList.add('error');
@@ -50,10 +62,13 @@ const checkElementValidity = function(element, elementContainer, regex, errorTex
     } else {
       console.log('data successfully submitted');
       element.classList.add('valid');
+      element.classList.remove('invalid');
       errorElement.textContent = "";
       errorElement.classList.remove('error');
     }
   });
+
+  return errorElement;
 }
 
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -61,4 +76,30 @@ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$
 
 checkElementValidity(email, emailContainer, emailRegex, 'Please enter a valid email address');
 
+checkElementValidity(password, passwordContainer, passwordRegex, 'Password must contain: 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special characters');
 
+const checkZipCodeValidity = function() {
+  let countryValue = country.value;
+
+  const zipCodeRegex = {
+    //6 digit zip codes for russia, china; 5 digits for us, germany, france
+    'ru': /^\d{6}$/, 
+    'us': /^\d{5}$/,
+    'cn': /^\d{6}$/,
+    'ge': /^\d{5}$/,
+    'fr': /^\d{5}$/,
+  }
+
+  country.addEventListener('change', (event) => {
+    let zipError = document.querySelector('.zipError');
+    zipContainer.removeChild(zipError);
+     
+    countryValue = country.value;
+    console.log(`country changed, value is ${countryValue}`);
+    checkElementValidity(zipCode, zipContainer, zipCodeRegex[countryValue], 'Invalid zip code');
+  });
+
+    checkElementValidity(zipCode, zipContainer, zipCodeRegex[countryValue], 'Invalid zip code');
+};
+
+checkZipCodeValidity();
